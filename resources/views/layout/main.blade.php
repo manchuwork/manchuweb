a03922cd2829d5fc54d67aee658d51c2606644b4
manchuwork <manchuwork@163.com>
2019-02-28 19:54:55 +0800

    移动js 导购尾部

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -7,20 +7,17 @@
     <link rel="stylesheet" href="/fonts/font-manchu">
     <link rel="stylesheet" href="/css/main">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-    <meta name="viewport"content="width=device-width,initial-scale=1.0,maximum-scale=1,user-scalable=no" />
-
-    <script src="/js/jquery"></script>
-    {{--<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/core.js"></script>--}}
+    <meta name="viewport"content="width=device-width,initial-scale=1.0" />
     <link rel="stylesheet" href="/css/mini">
     <title>@yield('title','Manchu 满语 ᠮᠠᠨᠵᡠ') Manchu 满语 ᠮᠠᠨᠵᡠ ᡤᡳᠰᡠᠨ manju gisun manchu.work</title>
-    @include("layout.tj")
 </head>
 <body class="container">
 @include("layout.nav")
 {{--@include("layout.welcome")--}}
-
 @yield("content")
 @include("layout.share")
+<script src="/js/jquery"></script>
+@include("layout.tj")
 @include("layout.footer")
 </body>
 </html>
